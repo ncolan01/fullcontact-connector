865a5169e9e12a89b2eabe448a1445e1bfa073ed
Nico <ncolangiuli@zendesk.com>
2014-12-23 00:07:12 +0100

    Update app..js

--- app.js
+++ app.js
@@ -1,14 +1,46 @@
 (function() {
 
-  return {
-    events: {
-      'app.activated':'showContactPage'
-    },
+    return {
 
-    showContactPage: function() {
-      console.log("TEST");
-      this.switchTo('contactPage');
-    }
-  };
+        events: {
+            'app.activated': 'getRequesterEmail'
+        },
+
+
+        //getRequesterEmail retrieve the ticket's requester email via the data API 
+        getRequesterEmail: function() {
+
+            var requesterEmail = this.ticket().requester().email();
+
+            if (requesterEmail) {
+
+                this.lookUpByEmail();
+
+            } else {
+
+                this.getFullRequesterObject();
+
+
+
+            }
+        },
+
+
+
+        getFullRequesterObject: function() {
+
+        	//this function will retrieve the fullcontact object
+
+            console.log("getFullRequesterObject reached ");
+
+        },
+
+        lookUpByEmail: function() {
+
+            console.log("lookUpByEmail reached");
+
+        }
+
+    };
 
 }());
